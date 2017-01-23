f79afb02b116efaff04a06fe3c25f7b8b0155099
Kyle Tulod <tulod.kyle.m@gmail.com>
2017-01-23 12:01:47 -0500

    Done.

---
+++ spaceship.js
@@ -0,0 +1,22 @@
+function Spaceship (name, crewArray, phasers, shields) {
+  this.name = name
+  this.crew = crewArray
+  this.phasers = phasers
+  this.shields = shields
+  this.cloaked = false
+  this.warpDrive = "disengaged"
+  if (this.crew.length === 0) {
+    this.docked = true
+  } else {
+    this.docked = false
+  }
+  this.phasersCharge = "uncharged"
+
+  // this.crew.forEach (this.crew.currentShip.bind(this)) {
+  //
+  // }
+
+  for (var i = 0; i < this.crew.length; i++) {
+    this.crew[i].currentShip = this
+  }
+}
